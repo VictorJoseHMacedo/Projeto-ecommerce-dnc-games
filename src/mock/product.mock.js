3d952f18abadebc4c5a5d99b971fbcc1e8f729c5
Victor José <112560183+VictorJoseHMacedo@users.noreply.github.com>
2023-04-21 10:53:03 -0300

    Update product.mock.js

--- src/mock/product.mock.js
+++ src/mock/product.mock.js
@@ -1,7 +1,7 @@
 export const PRODUCTS_MOCK = [
     {
       id: 1,
-      imgPath: "product-1.png",
+      imgPath: "/product-1.png",
       imgPathDetail: "../../../public/product-1.png",
       title: "Microsoft Xbox Series X | Preto",
       price: "R$ 4.500,00",
@@ -9,7 +9,7 @@ export const PRODUCTS_MOCK = [
       },
     {
       id: 2,
-      imgPath: "product-2.png",
+      imgPath: "/product-2.png",
       imgPathDetail: "../../../public/product-2.png",
       title: "Microsoft Xbox Series S | Branco",
       price: "R$ 2.200,00",
@@ -17,7 +17,7 @@ export const PRODUCTS_MOCK = [
     },
     {
       id: 3,
-      imgPath: "product-3.png",
+      imgPath: "/product-3.png",
       imgPathDetail: "../../../public/product-3.png",
       title: "Nintendo Switch com Jogo",
       price: "R$ 2.500,00",
@@ -25,7 +25,7 @@ export const PRODUCTS_MOCK = [
     },
     {
       id: 4,
-      imgPath: "product-4.png",
+      imgPath: "/product-4.png",
       imgPathDetail: "../../../public/product-4.png",
       title: "Jogo God of War Playstation",
       price: "R$ 100,00",
@@ -33,7 +33,7 @@ export const PRODUCTS_MOCK = [
     },
     {
       id: 5,
-      imgPath: "product-5.png",
+      imgPath: "/product-5.png",
       imgPathDetail: "../../../public/product-5.png",
       title: "Jogo Uncharted: The Nathan Drake Collection",
       price: "R$ 79,90",
@@ -42,7 +42,7 @@ export const PRODUCTS_MOCK = [
      },
     {
       id: 6,
-      imgPath: "product-6.png",
+      imgPath: "/product-6.png",
       imgPathDetail: "../../../public/product-6.png",
       title: "Jogo Gran Turismo",
       price: "R$ 99,50",
